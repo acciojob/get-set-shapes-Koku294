744d78f927417043331c7009a59d4f9d46051f06
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-02-23 06:25:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,16 +21,16 @@ class Rectangle {
   getArea(): number {
     return this._width * this._height;
   }
+}
+
 class Square extends Rectangle {
   constructor(side: number) {
     // Calls the parent constructor with both width and height set to the side length
     super(side, side);
-  }}
+  }
 
   // Method to calculate perimeter
   getPerimeter(): number {
     return 4 * this.width; // Since width == height for square
   }
-
 }
-
